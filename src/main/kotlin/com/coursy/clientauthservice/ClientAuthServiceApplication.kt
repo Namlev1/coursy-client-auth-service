b81472ddc839cf1402fa4b86e46385980606894e
namlev <mikipatkowski@protonmail.com>
2025-05-22 11:22:47 +0200

    Change class name, add "init complete" log message

--- src/main/kotlin/com/coursy/clientauthservice/ClientAuthServiceApplication.kt
+++ src/main/kotlin/com/coursy/clientauthservice/ClientAuthServiceApplication.kt
@@ -1,10 +1,26 @@
 package com.coursy.clientauthservice
 
+import jakarta.annotation.PreDestroy
+import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Profile
+import org.springframework.core.env.Environment
 
 @SpringBootApplication
-class ClientAuthServiceApplication
+class ClientAuthServiceApplication(
+    private val environment: Environment
+) {
+    private val logger = LoggerFactory.getLogger(ClientAuthServiceApplication::class.java)
+
+    @Profile("init")
+    @PreDestroy
+    fun destroy() {
+        if (environment.activeProfiles.contains("init")) {
+            logger.info("Application initialization complete")
+        }
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<ClientAuthServiceApplication>(*args)
--- src/main/kotlin/com/coursy/clientauthservice/setup/RoleInit.kt
+++ src/main/kotlin/com/coursy/clientauthservice/setup/RoleInit.kt
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service
 
 @Service
 @Profile("init")
-class RoleSetup {
+class RoleInit {
     @PostConstruct
     fun init() {
         println("role setup here")
