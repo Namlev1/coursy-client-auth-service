706b8a25ad7a2aaa02848a2d6873e9e81460e1e0
namlev <mikipatkowski@protonmail.com>
2025-08-06 18:04:58 +0200

    Add ROLE_ prefix

--- src/main/kotlin/com/coursy/users/model/Role.kt
+++ src/main/kotlin/com/coursy/users/model/Role.kt
@@ -1,10 +1,18 @@
 package com.coursy.users.model
 
+import arrow.core.Either
+import org.springframework.security.core.GrantedAuthority
+
 enum class Role {
-    HOST_OWNER,
-    HOST_ADMIN,
-    TENANT,
-    PLATFORM_OWNER,
-    PLATFORM_ADMIN,
-    PLATFORM_USER
+    ROLE_HOST_OWNER,
+    ROLE_HOST_ADMIN,
+    ROLE_TENANT,
+    ROLE_PLATFORM_OWNER,
+    ROLE_PLATFORM_ADMIN,
+    ROLE_PLATFORM_USER
+}
+
+fun GrantedAuthority.toRole(): Either<Throwable, Role> {
+    val roleString = this.authority.removePrefix("ROLE_")
+    return Either.catch { Role.valueOf(roleString) }
 }
